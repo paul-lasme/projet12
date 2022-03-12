52524fabf4092be975efa9ecbe7f766fb6df4124
paul-lasme <98124935+paul-lasme@users.noreply.github.com>
2022-03-12 22:26:35 +0100

    modifs du fichier app

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #APP FLASK (commande : flask run)
 # Partie formulaire non utilisée (uniquement appel à l'API)
 
-from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
-from flask_wtf import Form
-from wtforms.fields import StringField,BooleanField, PasswordField, TextAreaField
-from wtforms.widgets import TextArea
-import validators
-from wtforms.validators import DataRequired
-from toolbox.predict import *
-import pandas as pd
-import xgboost
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning)
-import os
+from flask import Flask, jsonify
+#from flask_wtf import Form
+#from wtforms.fields import StringField,BooleanField, PasswordField, TextAreaField
+#from wtforms.widgets import TextArea
+#import validators
+#from wtforms.validators import DataRequired
+#from toolbox.predict import *
+#import pandas as pd
+#import xgboost
+#import warnings
+#warnings.filterwarnings("ignore", category=UserWarning)
+#import os
 #import socketio
 
 
@@ -20,49 +20,19 @@ import os
 # App config.
 DEBUG = True
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b7776a'
+#app.config.from_object(__name__)
+#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b7776a'
 
-#formulaire d'appel à l'API (facultatif)
-class SimpleForm(Form):
-    form_id = StringField('formulaire_id:', validators=[DataRequired()])
-   
-@app.route('/formulaire_id', methods=['GET', 'POST'])
-def form():
-    form = SimpleForm(request.form)
-    print(form.errors)
-    
-    if request.method == 'POST':
-        form_id=request.form['formulaire_id']
-        print(form_id)
-        return(redirect('credit/'+form_id)) 
-    
-    if form.validate():
-        # Affichage de l'ID
-        flash('Vous avez demandé l\'ID : ' + form_id)
-        redirect('')
-    else:
-        flash('Veuillez compléter le champ. ')
-    
-    return render_template('formulaire_id.html', form=form)
-
-
-# Chargement des données
-#pathname = os.path.join("root", "directory1", "directory2")
-
-
-path_df = "traindata.csv"
-dataframe = pd.read_csv(path_df)
-
-@app.route('/credit/<form_id>', methods=['GET'])
-def credit(form_id):
+@app.route('/')
+@app.route('/credit/', methods=['GET'])
+def credit():
    
     #calcul prédiction défaut et probabilité de défaut
-    prediction, proba = predict_flask(form_id, dataframe)
+    #prediction, proba = predict_flask(form_id, dataframe)
 
     dict_final = {
-        'prediction' : int(prediction),
-        'proba' : float(proba)
+        'prediction' : 1,
+        'proba' : 0.2
         }
 
     print('Nouvelle Prédiction : \n', dict_final)
