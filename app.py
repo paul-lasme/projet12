fba80668a13f782b9c0370e3500cd88581e5f933
paul-lasme <98124935+paul-lasme@users.noreply.github.com>
2022-03-13 01:39:07 +0100

    modif de Procfile

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ visit_cnt=0
 def hello_world():
     global visit_cnt
     visit_cnt+=1
-     return jsonify(message='Congrats! you have deployed your first web service',time=datetime.now(),totoal_visit_count=visit_cnt)
+    return jsonify(message='Congrats! you have deployed your first web service',time=datetime.now(),totoal_visit_count=visit_cnt)
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0',port=my_port)
